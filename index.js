5b49e964a1dfd5015dc47e970d2a825694c8feef
kalush81 <krzysiek.kaluza@gmail.com>
2019-10-04 10:51:11 +0200

    Remove node_modules

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const Sequelize =  require('sequelize');
+
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres';
+
+const db = new Sequelize(databaseUrl);
+
+db.sync()
+.then(()=> console.log('db synced'))
+.catch(console.error)
