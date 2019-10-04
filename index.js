936143998d17baee239afe4480ca789470297a8d
kalush81 <krzysiek.kaluza@gmail.com>
2019-10-04 12:07:59 +0200

    test json-see

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Sequelize =  require('sequelize');
 const bodyParser = require('body-parser');
+const Sse =  require('json-sse')
+
+const stream = new Sse();
 
 const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres';
 
@@ -29,13 +32,27 @@ app.get('/', (req, res) => {
     res.send('hello world')
 }) 
 
-app.post('/message', (req, res) => {
+app.post('/message', async (req, res) => {
     console.log('got req on /message', req.body);
     const { message } = req.body
-    const entity = Chatroom.create({
+    const entity = await Chatroom.create({
         message,
         user: 'it s me'
     })
+    const room = await Chatroom.findAll()
+    const data =  JSON.stringify(room)
+    stream.send(data)
+    
     res.status(200)
     res.send('thanks for your message')
+})
+
+app.get('/stream', async (req, res) => {
+    console.log('got a req from /stream');  
+    console.log('msgs in this room are', data)
+
+    const room = await Chatroom.findAll()
+    const data =  JSON.stringify(room)
+    stream.updateInit(data)
+    stream.init(req, res)
 })
