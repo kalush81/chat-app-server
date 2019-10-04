ce535abce5bebea3c126ba5f08286a234213bc92
kalush81 <krzysiek.kaluza@gmail.com>
2019-10-04 10:55:41 +0200

    Initial server and sync database

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const Sequelize =  require('sequelize');
 
 const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres';
 
+const port = process.env.PORT || 5000
+const app = express()
+
+app.listen(port, () => console.log(`listenining on ${port}`))
+
 const db = new Sequelize(databaseUrl);
 
 db.sync()
